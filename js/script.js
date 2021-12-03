cc436693beac9101e516c1808cab00e44e925189
Gtp87 <fabio.giannitrapani@gmail.com>
2021-12-03 15:42:58 +0100

    creati 5 num casuali

--- js/script.js
+++ js/script.js
@@ -2,4 +2,34 @@
 // Da lì parte un timer di 30 secondi.
 // Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-console.log('prova script');
+
+
+let randNumContainer = document.querySelector('.numbers');
+let numRandomArray = [];
+
+// funzione numeri casuali
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); 
+}
+
+// prendo 5 numeri casuali da 1 a 100
+
+for (let index = 0; index < 5; index++) {
+    let randNum = getRandomIntInclusive(1, 100);
+
+    // verifico siano numeri unici
+    while (numRandomArray.includes(randNum)) {
+        randNum = getRandomIntInclusive(1, 100);
+    }
+    
+    // inserisco numeri in array
+    numRandomArray.push(randNum);
+}
+
+console.log(numRandomArray);
+
+// stampo in pagina
+randNumContainer.append(numRandomArray);
